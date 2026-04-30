62f786c8b5413b2c0bdd063b36de49a8af850885
anthonyGuanga <ninfrad23@gmail.com>
2026-04-30 17:39:19 +0200

    add nuevo componente , se add funciones a favoritos

---
+++ frontend-angular/match-pet/src/app/components/mis-favoritos.component/mis-favoritos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-favoritos.component',
+  imports: [],
+  templateUrl: './mis-favoritos.component.html',
+  styleUrl: './mis-favoritos.component.css',
+})
+export class MisFavoritosComponent {
+
+}
--- frontend-angular/match-pet/src/app/services/favoritos.ts
+++ frontend-angular/match-pet/src/app/services/favoritos.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Favoritos {
-  
+  // 👇 URL base sacada de tu captura de pantalla 👇
+  private apiUrl = 'http://localhost/RefugioAnimalesMatchPet/backend-php/api/favoritos';
+
+  constructor(private http: HttpClient) { }
+
+  // Método auxiliar para obtener los headers con el token
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  getFavoritos(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/listar_favoritos.php`, { headers: this.getHeaders() });
+  }
+
+  agregarFavorito(id_animal: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/agregar_favorito.php`, { id_animal }, { headers: this.getHeaders() });
+  }
+
+  eliminarFavorito(id_animal: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/eliminar_favorito.php`, { id_animal }, { headers: this.getHeaders() });
+  }
 }
